32f3273f60bc002c191548dd024ab8d89c468ff2
Kindelik <kindelik@yandex.ru>
2022-11-20 22:13:32 +0300

    example005 debug

--- example_005_ifelse/Program.cs
+++ example_005_ifelse/Program.cs
@@ -1,11 +1,11 @@
 ﻿
-Console.WriteLine("Введите имя пользователя");
-string username = Console.ReadLine;
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
 
-if (username.ToLower == "Маша");
+if (username.ToLower() == "kindelik")
 {
-    Console.WriteLine ("Ура, это же Маша!!!");
-} 
+   Console.WriteLine ("Ура, это же Kindelik!!!");
+}
 else
 {
   Console.Write("Hello, ");
