becf339eda0e987f68b915ac781942c40f15cbd7
Kindelik <kindelik@yandex.ru>
2022-11-22 23:06:43 +0300

    Ex11

--- example_011_ArrayLibrary/Program.cs
+++ example_011_ArrayLibrary/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection)            //Метод - Создать рандомный массив
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10); // Положить новое рандомное число в индекс из диапозона 1-10
+        index++;
+    }
+}
+
+void PrintArray(int[] col)             //Метод - Вывести массив в терминал
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write($"{col[position]} " );
+        position++;
+    }
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
